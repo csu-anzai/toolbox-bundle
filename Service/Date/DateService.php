48ea688817e5d71ab3d01eeae8062f19195c380f
Aurelien Tournayre <a.tournayre@aroban.com>
2019-08-15 22:33:11 +0200

    DateService : create from datetime

--- Service/Date/DateService.php
+++ Service/Date/DateService.php
@@ -3,7 +3,20 @@
 namespace Atournayre\ToolboxBundle\Service\Date;
 
 use Carbon\Carbon;
+use DateTimeInterface;
 
 class DateService extends Carbon
 {
+    /**
+     * @param DateTimeInterface $dateTime
+     *
+     * @return DateService|Carbon
+     */
+    public function createFromDateTime(DateTimeInterface $dateTime) {
+        return self::createFromDate(
+            $dateTime->format('Y'),
+            $dateTime->format('m'),
+            $dateTime->format('d')
+        );
+    }
 }
